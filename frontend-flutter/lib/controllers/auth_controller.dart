e946fbe04e7bd3eb212496a79ceb39284910f916
Adryan Pratama <adryanpratama168@gmail.com>
2025-05-20 09:25:23 +0700

    Update Login/Register
    Connect API postgreSQL

--- frontend-flutter/lib/controllers/auth_controller.dart
+++ frontend-flutter/lib/controllers/auth_controller.dart
@@ -1,14 +1,15 @@
 import 'package:get/get.dart';
-import 'package:login_sqflite_getx/services/database_helper.dart';
+import 'package:login_sqflite_getx/services/api_services.dart';
 import 'package:login_sqflite_getx/pages/home_page.dart';
 import 'package:login_sqflite_getx/pages/login_page.dart';
 import 'package:flutter/material.dart';
 
 class AuthController extends GetxController {
   var isLoggedIn = false.obs;
+  final ApiService apiService = ApiService();
 
   login(String username, String password) async {
-    var result = await DatabaseHelper.instance.login(username, password);
+    var result = await apiService.login(username, password);
     if (result != null) {
       isLoggedIn.value = true;
       Get.snackbar(
@@ -30,25 +31,25 @@ class AuthController extends GetxController {
     }
   }
 
- register(String username, String password) async {
-  var result = await DatabaseHelper.instance.register(username, password);
-  if (result != null) {
-    Get.snackbar(
-      "Registration Success",
-      "Your account has been created successfully",
-      snackPosition: SnackPosition.BOTTOM,
-      backgroundColor: Colors.green[100],
-      colorText: Colors.green[800],
-    );
-    Get.to(() => LoginPage());
-  } else {
-    Get.snackbar(
-      "Registration Failed",
-      "Username might already exist",
-      snackPosition: SnackPosition.BOTTOM,
-      backgroundColor: Colors.red[100],
-      colorText: Colors.red[800],
-    );
+  register(String username, String password) async {
+    var result = await apiService.register(username, password);
+    if (result != null) {
+      Get.snackbar(
+        "Registration Success",
+        "Your account has been created successfully",
+        snackPosition: SnackPosition.BOTTOM,
+        backgroundColor: Colors.green[100],
+        colorText: Colors.green[800],
+      );
+      Get.to(() => LoginPage());
+    } else {
+      Get.snackbar(
+        "Registration Failed",
+        "Username might already exist",
+        snackPosition: SnackPosition.BOTTOM,
+        backgroundColor: Colors.red[100],
+        colorText: Colors.red[800],
+      );
+    }
   }
-}
 }
